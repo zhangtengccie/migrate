b498ecd27e3d53c850b4dad872d44260c56d07f9
zhangtengccde <zhangtengccie@gmail.com>
2021-10-16 09:23:04 +0800

    develop A change

--- re_IP.py
+++ re_IP.py
@@ -2,6 +2,7 @@
 # -*- coding=utf8 -*- 
 # Creater:zhangteng
 ############################
+############AAAAAAAAAAAA################
 import re
 
 string1 = 'Port-channel1.189     192.168.189.254  YES   CONFIG  up         up   '
